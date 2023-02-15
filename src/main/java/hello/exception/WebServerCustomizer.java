27f36a64a3dd8e4cc9e23e3175d468e2013343e3
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-02-15 17:31:23 +0900

    스프링 부트 기본 API 오류 처리

--- src/main/java/hello/exception/WebServerCustomizer.java
+++ src/main/java/hello/exception/WebServerCustomizer.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 
-@Component
+//@Component
 public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
 
     @Override
